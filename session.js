6e92a359f838cecfc6be3602a8bbe5655656074a
sirisomasampath1 <sirisomasampath1@gmail.com>
2024-11-27 09:41:27 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=DEs0EDjT#iWI8ZK_O5yabAxqA3n_GFZBruOneO5zRvlVNSTMgoU0",
 };
